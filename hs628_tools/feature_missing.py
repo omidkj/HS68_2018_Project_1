9b4865e96b1642d2db45d7def08c37765b15e6d1
omidkj <omid.khazaei@gmail.com>
2018-07-18 21:15:25 -0700

    Finding unimportant features: missing values
    - This method receives a ndarray with a specified threshold (between 0 and 1)
      for missing values

--- hs628_tools/feature_missing.py
+++ hs628_tools/feature_missing.py
@@ -1,73 +1,32 @@
 
-# coding: utf-8
-
-# In[2]:
-
 
 import numpy as np
+class FeatureRemover():
 
+    def __init__(self):
 
-# In[ ]:
-
-
-def __init__(self):
-        
-        # Dataframes recording information about features to remove
+        # Numpy arrays recording information about features to remove
         self.missing_col = None
         self.single_unique = None
         self.collinear = None
         self.low_importance = None
-        
+
         self.feature_importances = None
-       
-    
 
 
-# In[34]:
+    def which_missing(self, data, missing_thresh):
 
+        self.missing_thresh = missing_thresh
 
-a = np.array([[1,2,3], [4,5,6], [np.nan, np.nan, np.nan], [np.nan,8,np.nan]])
+        # Calculate the fraction of missing in each column
+        missing_series = (len(a) - np.sum(data==data, axis=0)) / float(len(data))
 
+        # Find the columns with a missing percentage above the threshold
+        missing_col = np.where(missing_series > missing_thresh)
 
-# In[35]:
+        missing_col = missing_col[0]
 
+        self.missing_col = missing_col
 
-a.shape[0]
-
-
-# In[36]:
-
-
-missing_entries = (len(a) - np.sum(a==a, axis=0)) / float(len(a))
-missing_entries
-
-
-# In[37]:
-
-
-ms_th = 0.30
-b= missing_entries > ms_th
-missing_col = np.where(b)
-missing_col[0]
-
-
-# In[ ]:
-
-
-def which_missing(self, data, missing_thresh):
-       
-       self.missing_thresh = missing_thresh
-
-       # Calculate the fraction of missing in each column 
-       missing_series = (len(a) - np.sum(data==data, axis=0)) / float(len(data))
-       
-       # Find the columns with a missing percentage above the threshold
-       missing_col = np.where(missing_series > missing_thresh)
-
-       missing_col = missing_col[0]
-
-       self.missing_col = missing_col
-       
-       print('%d columns with greater than %0.2f missing values.\n' % (len(self.missing_col), self.missing_thresh))
-       
-
+        print('%d columns with greater than %0.2f missing values.\n' % (len(self.missing_col), self.missing_thresh))
+        return missing_col
