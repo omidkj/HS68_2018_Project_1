14d7881e5e365303f0497cb2fa82bd2c9ad3d335
omidkj <omid.khazaei@gmail.com>
2018-07-22 13:31:53 -0700

    Adding comments and notes to the code.
    - notes to reference the code in future
    - Describing the modules and functions

--- hs628_tools/feature_missing.py
+++ hs628_tools/feature_missing.py
@@ -7,7 +7,18 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
 
 class FeatureRemover():
+    """This Class is created to perform feature removal tools for data cleaning.
 
+    #There are four methods in this class:
+
+        1. which_missing: Returns columns with a missing percentage greater than a specified threshold
+        2. single_value: Returns columns with a single unique value
+        3. colinear: Returns collinear columns with a correlation greater than a specified correlation threshold
+        4. low_importance: Returns features that are ranked low importance in the Random Forest Regressor or Classifier
+    #Notes:
+         using the idea adapted from: https://github.com/WillKoehrsen/feature-selector/blob/master/Feature%20Selector%20Development.ipynb
+         but coding with numpy instead of Pandas and using different techniques.
+    """
     def __init__(self):
 
         # Numpy arrays recording information about features to remove
@@ -20,6 +31,19 @@ class FeatureRemover():
 
     def which_missing(self, data, missing_thresh=0.4):
 
+        """
+        Find columns with a fraction of missing data more than pre-dedfined threshold
+            -----------
+            Parameters:
+                data: numpy ndarray
+                        observations and features
+                missing_thresh: float between 0 and 1
+                        Pre-defined threshold set by the user, default value = 0.4
+            -----------
+            Returns:
+                An array containing the indices of columns with missing values more than the threshold
+        """
+
         self.missing_thresh = missing_thresh
 
         # Calculating the fraction of missing values in each column
@@ -27,7 +51,7 @@ class FeatureRemover():
 
         # Find the columns with a missing fraction above the threshold
         missing_col = np.where(missing_series > missing_thresh)
-
+        # List of indices is located in the 0 element of missing_col array
         missing_col = missing_col[0]
 
         self.missing_col = missing_col
@@ -41,8 +65,22 @@ class FeatureRemover():
 
 
     def single_value(self, data):
+
+        """
+        Find columns that have only a single value.
+            -----------
+            Parameters:
+                data: numpy ndarray
+                        observations and features
+            -----------
+            Returns:
+                A list containing the indices of columns with only a single value
+        """
+
         single_unique = []
         for i in range(data.shape[1]):
+            # finding the unique value of each column and if the lenghth of that equals 1 means
+            # that feature has only one sibgle value, then save the index of that column in single_unique
             if len(np.unique(data[:,i], axis=0)) == 1:
                 single_unique += [i]
 
@@ -57,16 +95,36 @@ class FeatureRemover():
 
 
     def colinear(self, data, corr_thresh = 0.9):
+
+        """
+        Find columns with correlation coefficient value more than specified value
+            -----------
+            Parameters:
+                data: numpy ndarray
+                        observations and features
+                corr_thresh: float between 0 and 1
+                        Pre-defined threshold set by the user, default value = 0.9
+            -----------
+            Returns:
+                A list of tuples containing a pair of column indices with
+                correlation coefficient value more than specified value
+        """
+
         self.corr_thresh = corr_thresh
+        # creating a matix for correlation coefficient
         corr_matrix = np.zeros((data.shape[1], data.shape[1]))
+
         for i in range(data.shape[1]):
             for j in range(data.shape[1]):
+                #Calculating the corrcoef of each pair and save in corr_matrix
                 corr_matrix[i, j] = np.corrcoef(data[:,i],data[:,j])[0,1]
+        #finding pairs with corrcoef more than corr_thresh and save them as a tuple in corr_pairs
         corr_pairs = []
         for i in range(corr_matrix.shape[0]):
             for j in range(i+1, corr_matrix.shape[0]):
                 if abs(corr_matrix [i,j]) > self.corr_thresh:
                     corr_pairs += [(i,j)]
+
         print ('%d features with a correlation greater than %0.2f:\n\n' %(len(corr_pairs), self.corr_thresh))
         for i in corr_pairs:
             print('columns #%d and #%d have a correlation value of %s \n' %(i[0],i[1], corr_matrix[i[0],i[1]]))
@@ -76,28 +134,50 @@ class FeatureRemover():
 
     def low_importance(self, features, target, target_type = "classification", importance_thresh = 1/features.shape[1]):
 
+        """
+        Find columns with their normalized importance value below pre-defined threshold using RandomForestRegressor or
+        RandomForestClassifier based on the target_type.
+            -----------
+            Parameters:
+                features: numpy ndarray
+                        observations and features
+                target: array, shape = (1, )
+                        target can be binary if target_type is classification or continuous if target_type is regression
+                target_type: string
+                        defines the RandomForest method if target is continuous or binary. default value = "classification"
+                importance_thresh: float between 0 and 1
+                        Pre-defined threshold set by the user, default value = 1/number of the features
+            -----------
+            Returns:
+                A ranked (sorted) list containing indices of fetaures  with importance value below importance_thresh
+        """
+
         self.features = features
         self.target = target
         self.importance_thresh = importance_thresh
-
+        # Performing the train_test_split
         X_train, X_test, y_train, y_test = train_test_split(self.features, self.target, test_size = 0.25)
-
+        # Selecting the model based on target_type
         if target_type == "regression":
             model = RandomForestRegressor()
         elif target_type == "classification":
             model = RandomForestClassifier()
         else:
             raise ValueError('Target type must be "regression" or "classification" ')
-
+        #Fitting the model
         model.fit(X_train, y_train)
         feature_importance = model.feature_importances_
+        #normalizing the feature importance values
         feat_imp_norm = feature_importance / np.sum(feature_importance)
+        Creating a template (copy) to sort low important features
         feat_imp_temp = feat_imp_norm.copy()
         low_importance = []
 
         for i in range(feat_imp_temp.shape[0]):
+            #finding the index of the minimum value and save it to low_importance
             low_importance += [feat_imp_norm.argmin()]
-            feat_imp_norm [feat_imp_norm.argmin()]= 10
+            #set the minimum to a large number in feat_imp_norm to find the next minimum value
+            feat_imp_norm [feat_imp_norm.argmin()]= 1000
 
         for i in range(len(low_importance)):
             if feature_importance[low_importance[i]] < self.importance_thresh:
