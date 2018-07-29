6bcc9c9319f8b49360f3fae57a584ca1c36b303b
omidkj <omid.khazaei@gmail.com>
2018-07-28 20:32:28 -0700

    adding Kfold cross validation
    - removing train_test_split
    - adding if __name__ == "__main__" to include some examples of creating an object from class and calling methods.

--- hs628_tools/feature_missing.py
+++ hs628_tools/feature_missing.py
@@ -2,12 +2,7 @@ import numpy as np
 from numpy.core.multiarray import ndarray
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-
-
-import numpy as np
-from numpy.core.multiarray import ndarray
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
 
 
 class FeatureRemover:
@@ -91,14 +86,14 @@ class FeatureRemover:
 
         return np.array(self.single_unique)
 
-    def colinear(self, data, corr_thresh=0.9):
+    def colinear(self, train_data, corr_thresh=0.9):
         # type: (ndarray, float) -> ndarray
 
         """
         Find columns with correlation coefficient value more than specified value
             -----------
             Parameters:
-                data: numpy ndarray, shape = [n_samples, n_features]
+                train_data: numpy ndarray, shape = [n_samples, n_features]
                       observations and features
                 corr_thresh: float between 0 and 1
                       Pre-defined threshold set by the user, default value = 0.9
@@ -109,12 +104,12 @@ class FeatureRemover:
         """
 
         # creating a matix for correlation coefficient
-        corr_matrix = np.zeros((data.shape[1], data.shape[1]))
+        corr_matrix = np.zeros((train_data.shape[1], train_data.shape[1]))
 
-        for i in range(data.shape[1]):
-            for j in range(data.shape[1]):
+        for i in range(train_data.shape[1]):
+            for j in range(train_data.shape[1]):
                 # Calculating the corrcoef of each pair and save in corr_matrix
-                corr_matrix[i, j] = np.corrcoef(data[:, i], data[:, j])[0, 1]
+                corr_matrix[i, j] = np.corrcoef(train_data[:, i], train_data[:, j])[0, 1]
         # finding pairs with corrcoef more than corr_thresh and save them as a tuple in corr_pairs
         corr_pairs = []
         for i in range(corr_matrix.shape[0]):
@@ -128,7 +123,7 @@ class FeatureRemover:
         self.collinear_col = np.array(corr_pairs)
         return self.collinear_col
 
-    def low_importance(self, features, target, target_type="classification", importance_thresh=1 / features.shape[1]):
+    def low_importance(self, features, target, target_type="classification", importance_thresh=0.05):
         # type: (ndarray, ndarray, str, float) -> ndarray
 
         """
@@ -137,7 +132,7 @@ class FeatureRemover:
             -----------
             Parameters:
                 features :  numpy ndarrays, , shape = [n_samples, n_features]
-                            observations and features
+                            Train data, observations and features
 
                 target:     numpy ndarrays, shape = (1, ) target can be binary if target_type is classification or
                             continuous if target_type is regression
@@ -152,37 +147,55 @@ class FeatureRemover:
                 A ranked (sorted) list containing indices of features  with importance value below importance_thresh
 
         """
-        # Performing the train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.25)
-        # type: (ndarray, ndarray, ndarray, ndarray)
         # Selecting the model based on target_type
+
         if target_type == "regression":
             model = RandomForestRegressor()
         elif target_type == "classification":
             model = RandomForestClassifier()
         else:
             raise ValueError('Target type must be "regression" or "classification" ')
-        # Fitting the model
-        model.fit(X_train, y_train)
-        feature_importance = model.feature_importances_
+
+        feature_importance = [0] * features.shape[1]
+        # creating k-fold cross validation with 10 folds
+        skf = StratifiedKFold(n_splits=10, random_state=None, shuffle=False)
+        for train_index, test_index in skf.split(features, target):
+            features_train = features[train_index]
+            targets_train = target[train_index]
+            features_test = features[test_index]
+            targets_test = target[test_index]
+            # Fitting the model
+            model.fit(features_train, targets_train)
+            model.score(features_test, targets_test)
+            feature_importance += model.feature_importances_
         # normalizing the feature importance values
-        feat_imp_norm = feature_importance / np.sum(feature_importance)
+        feat_imp_norm = feature_importance / skf.get_n_splits(features, target)
         # Creating a template (copy) to sort low important features
         feat_imp_temp = feat_imp_norm.copy()
         low_imp = []
 
         for i in range(feat_imp_temp.shape[0]):
             # finding the index of the minimum value and save it to low_importance
-            low_imp += [feat_imp_norm.argmin()]
+            low_imp += [feat_imp_temp.argmin()]
             # set the minimum to a large number in feat_imp_norm to find the next minimum value
-            feat_imp_norm[feat_imp_norm.argmin()] = 1000
+            feat_imp_temp[feat_imp_temp.argmin()] = 1000
 
         low_importance = []
         for i in range(len(low_imp)):
-            if feature_importance[low_imp[i]] < importance_thresh:
-                print ('Column #%d is #%d in the list of low importance features with the importnace value of %s \n' % (
-                low_imp[i], i + 1, feature_importance[low_imp[i]]))
-                low_importance +=[low_imp[i]]
+            if feat_imp_norm[low_imp[i]] < importance_thresh:
+                print('Column #%d is #%d in the list of low importance features with the importnace value of %s \n' % (
+                    low_imp[i], i + 1, feat_imp_norm[low_imp[i]]))
+                low_importance += [low_imp[i]]
 
         self.low_importance_col = np.array(low_importance)
         return self.low_importance_col
+
+
+if __name__ == "__main__":
+    data_np = np.genfromtxt('data.csv', delimiter=',', skip_header=1, missing_values='None', filling_values=np.nan)
+    features_np = data_np[:, 0:8]
+    target_np = data_np[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(features_np, target_np, test_size=0.25)
+    frs = FeatureRemover()
+    correlated_indices = frs.colinear(X_train, corr_thresh=0.7)
+    low_importance_indices = frs.low_importance(X_train, y_train, importance_thresh=0.12)
